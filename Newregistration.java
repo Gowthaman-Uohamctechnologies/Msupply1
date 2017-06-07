00d0471324bf4c5a076307c3b836595785b234ed
Skpgithub123 <puttursunil@gmail.com>
2017-06-07 16:59:18 +0530

    Add files via upload

---
+++ Newregistration.java
@@ -0,0 +1,16 @@
+package app.e_commerce.com.msupply;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by uOhmac Technologies on 07-Jun-17.
+ */
+public class Newregistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
